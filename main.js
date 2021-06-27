286f62bc2732e9d607eed696aa4dc25173fa3b1b
PavelAvrusevich <p.avrusevich@gmail.com>
2021-06-27 20:12:20 +0300

    Store settings. Created Tray.

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-const { app, BrowserWindow, Menu } = require('electron');
-const log = require('electron-log');
+const path = require('path');
+const { app, BrowserWindow, Menu, ipcMain, Tray } = require('electron');
+const Store = require('./Store');
 
 // Set env
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'production';
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
+let tray;
+
+const store = new Store({
+    configName: 'user-settings',
+    defaults: {
+        settings: {
+            cpuOverload: 80,
+            alertFrequency: 5,
+        },
+    },
+});
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
@@ -16,10 +28,15 @@ function createMainWindow() {
         height: 500,
         icon: './assets/icons/icon.png',
         resizable: isDev ? true : false,
+        show: false,
         webPreferences: {
             contextIsolation: false,
             nodeIntegration: true,
         },
+    });
+
+    mainWindow.webContents.on('dom-ready', () => {
+        mainWindow.webContents.send('settings:get', store.get('settings'));
     });
 
     if (isDev) {
@@ -34,6 +51,41 @@ app.on('ready', () => {
 
     const mainMenu = Menu.buildFromTemplate(menu);
     Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('close', (e) => {
+        if (!app.isQuitting) {
+            e.preventDefault();
+            mainWindow.hide();
+        }
+
+        return true;
+    });
+
+    // Create a tray
+    const icon = path.join(__dirname, 'assets', 'icons', 'tray_icon.png');
+    tray = new Tray(icon);
+    tray.setToolTip('SysTop');
+    tray.on('click', () => {
+        if (mainWindow.isVisible() === true) {
+            mainWindow.hide();
+        } else {
+            mainWindow.show();
+        }
+    });
+
+    tray.on('right-click', () => {
+        const contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'Quit',
+                click: () => {
+                    app.isQuitting = true;
+                    app.quit();
+                },
+            },
+        ]);
+
+        tray.popUpContextMenu(contextMenu);
+    });
 });
 
 const menu = [
@@ -55,6 +107,12 @@ const menu = [
           ]
         : []),
 ];
+
+// Set setiings
+ipcMain.on('settings:set', (e, value) => {
+    store.set('settings', value);
+    mainWindow.webContents.send('settings:get', store.get('settings'));
+});
 
 app.on('window-all-closed', () => {
     if (!isMac) {
